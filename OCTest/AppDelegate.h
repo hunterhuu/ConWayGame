d83e3d4162000d2e9683b7b6e22e0c64b922292c
developer <developer@nippontsuneo-no-MacBook-Pro.local>
2017-10-19 13:48:38 +0900

    Initial Commit

---
+++ OCTest/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  OCTest
+//
+//  Created by developer on 2017/10/19.
+//  Copyright © 2017年 developer. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
